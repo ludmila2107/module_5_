bc8956899c39a027f7a37909367188ed9cfe7dee
Ludmila <lvminzulina@mail.ru>
2024-08-11 18:07:59 +0300

    module_5.3

---
+++ CustomNumber.py
@@ -0,0 +1,19 @@
+class CustomNumber:
+
+def__init__(self, CustomNumber):
+    self.value = CustomNumber
+
+
+#     def__add__(self, other):
+#        if isinstance(other, CustomNumber):
+# 	       return CustomNumber(self.value + other.value)
+#        else:
+# 	       return CustomNumber(self.value + other)
+# def__radd__(self, other):
+# return self.__add__(other)
+
+# a = CustomNumber(5)
+# b = CustomNumber(10)
+# print((a + b).value) # Вывод: 15
+# print((a + 2).value) # Вывод: 7
+# print((3 + a).value) # Вывод: 8
