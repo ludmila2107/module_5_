1699068b39dff3353789397bc15332dbd2d48cdf
Ludmila <lvminzulina@mail.ru>
2024-08-12 19:15:45 +0300

    module_5.3-1

--- module_5_3.py
+++ module_5_3.py
@@ -14,30 +14,31 @@ class House:
     def __str__(self):
         return f"Название: {self.name}, кол-во этажей:{self.number_of_floors}"
 
-def __eq__(self, other):
-    return self.number_of_floors == other.number_of_floors
-def __lt__(self, other):
-    return self.number_of_floors < other.number_of_floors
-def __le__(self, other):
-    return self.number_of_floors <= other.number_of_floors
-def __gt__(self, other):
-    return self.number_of_floors > other.number_of_floors
-def __ge__(self, other):
-    return self.number_of_floors >= other.number_of_floors
-def __ne__(self, other):
-    return self.number_of_floors != other.number_of_floors
-def __add__(self, value):
-    if isinstance(value, int):
-        self.number_of_floors += value
-        return self
-def __radd__(self, value):
-    if isinstance(value, int):
-        self.number_of_floors += value
-        return self
-def __iadd__(self, value):
-    if isinstance(value, int):
-        self.number_of_floors += value
-        return self
+    def __eq__(self, other):
+        return self.number_of_floors == other.number_of_floors
+    def __lt__(self, other):
+        return self.number_of_floors < other.number_of_floors
+    def __le__(self, other):
+        return self.number_of_floors <= other.number_of_floors
+    def __gt__(self, other):
+        return self.number_of_floors > other.number_of_floors
+    def __ge__(self, other):
+        return self.number_of_floors >= other.number_of_floors
+    def __ne__(self, other):
+        return self.number_of_floors != other.number_of_floors
+
+    def __add__(self, value):
+        if isinstance(value, int):
+            self.number_of_floors += value
+            return self
+    def __radd__(self, value):
+        if isinstance(value, int):
+            self.number_of_floors += value
+            return self
+    def __iadd__(self, value):
+        if isinstance(value, int):
+            self.number_of_floors += value
+            return self
 
 
 
