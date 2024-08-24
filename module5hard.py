936bdd1a9c48b5559540b5f75755cc57721fc9bf
Ludmila <lvminzulina@mail.ru>
2024-08-24 16:49:59 +0300

    module_6_-51

--- module5hard.py
+++ module5hard.py
@@ -1,3 +1,5 @@
+import time
+
 
 class User:
 	def __init__(self, nickname, password, age):
@@ -15,10 +17,10 @@ class Video:
 
 
 class UrTube:
-	def __init__(self, users, videos, current_user):
-		self.users = users
-		self.videos = videos
-		self.current_user = current_user
+	def __init__(self):
+		self.users = []
+		self.videos = []
+		self.current_user = None
 
 	def log_in(self, nickname, password):
 		for user in self.users:
@@ -39,20 +41,73 @@ class UrTube:
 		user = User(nickname, password, age)
 		self.users.append(user)
 		self.current_user = user
-def log_out(self):
-	self.current_user = None
-	print('Cброс текущего пользователя')
-def add(self, *video_object):
-	for video in videos:
-		if video.title == title:
-			self.videos.append(video_object)
-def get_videos(self, title):
-	videos_list = [video['name'] for video in self.videos if title in video['name']]
-	return videos_list
 
-def watch_video(self, title):
-	for video in videos:
-		if video.title != title:
-			break
+	def log_out(self):
+		self.current_user = None
+		print('Cброс текущего пользователя')
+
+	def add(self, *video_object):
+		for video in video_object:
+			if video not in self.videos:
+				self.videos.append(video)
+
+			else:
+				print('Такой фильм уже есть')
+
+	def get_videos(self, title):
+		title = title.lower()
+		video_list = []
+
+		for video in self.videos:
+			if title in video.title.lower():
+				video_list.append(video.title)
+		return video_list
+
+	def watch_video(self, title):
+		count = 0
+		if self.current_user == None:
+			print('Войдите в аккаунт, чтобы смотреть видео')
+			return
+		for video in self.videos:
+			if video.title == title:
+				if (video.adult_mode == True and self.current_user.age >= 18) or (video.adult_mode == False):
+					print('можно смотреть')
+					while video.duration > count:
+						time.sleep(1)
+						count += 1
+						print(count)
+
+					else:
+						print('Конец видео')
+						count = 0
+				else:
+					print('Вам нет 18 лет, пожалуйста покиньте страницу')
 
 
+
+
+ur = UrTube()
+v1 = Video('Лучший язык программирования 2024 года', 200)
+v2 = Video('Для чего девушкам парень программист?', 10, adult_mode=True)
+
+# Добавление видео
+ur.add(v1, v2)
+
+
+# Проверка поиска
+print(ur.get_videos('лучший'))
+print(ur.get_videos('ПРОГ'))
+
+# Проверка на вход пользователя и возрастное ограничение
+ur.watch_video('Для чего девушкам парень программист?')
+ur.register('vasya_pupkin', 'lolkekcheburek', 13)
+ur.watch_video('Для чего девушкам парень программист?')
+ur.register('urban_pythonist', 'iScX4vIJClb9YQavjAgF', 25)
+ur.watch_video('Для чего девушкам парень программист?')
+
+# Проверка входа в другой аккаунт
+ur.register('vasya_pupkin', 'F8098FM8fjm9jmi', 55)
+print(ur.current_user)
+
+# Попытка воспроизведения несуществующего видео
+ur.watch_video('Лучший язык программирования 2024 года!')
